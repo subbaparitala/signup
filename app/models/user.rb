d65090617022be50b0cfd3959d21a5146057cab0
subbaparitala <subbaparitala@gmail.com>
2019-01-09 01:22:07 -0600

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
  validates_presence_of   :avatar
  validates_integrity_of  :avatar
  validates_processing_of :avatar
+ validates_presence_of   :first_name
+ validates_presence_of   :last_name
 end
